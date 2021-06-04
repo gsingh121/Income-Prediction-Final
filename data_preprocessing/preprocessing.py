43bb174d1f266d66b51ce732ab8298aa89fc6ff3
gsingh121 <85227339+gsingh121@users.noreply.github.com>
2021-06-04 11:52:59 +0530

    Update preprocessing.py

--- data_preprocessing/preprocessing.py
+++ data_preprocessing/preprocessing.py
@@ -7,7 +7,7 @@ class Preprocessor:
     """
         This class shall  be used to clean and transform the data before training.
 
-        Written By: iNeuron Intelligence
+        Written By: Gourav Singh
         Version: 1.0
         Revisions: None
 
@@ -24,7 +24,7 @@ class Preprocessor:
                         Output: A pandas DataFrame after removing the spaces.
                         On Failure: Raise Exception
 
-                        Written By: iNeuron Intelligence
+                        Written By: Gourav Singh
                         Version: 1.0
                         Revisions: None
 
@@ -53,7 +53,7 @@ class Preprocessor:
                 Output: A pandas DataFrame after removing the specified columns.
                 On Failure: Raise Exception
 
-                Written By: iNeuron Intelligence
+                Written By: Gourav Singh
                 Version: 1.0
                 Revisions: None
 
@@ -79,7 +79,7 @@ class Preprocessor:
                         Output: Returns two separate Dataframes, one containing features and the other containing Labels .
                         On Failure: Raise Exception
 
-                        Written By: iNeuron Intelligence
+                        Written By: Gourav Singh
                         Version: 1.0
                         Revisions: None
 
@@ -104,7 +104,7 @@ class Preprocessor:
                                         returns the list of columns for which null values are present.
                                 On Failure: Raise Exception
 
-                                Written By: iNeuron Intelligence
+                                Written By: Gourav Singh
                                 Version: 1.0
                                 Revisions: None
 
@@ -138,7 +138,7 @@ class Preprocessor:
                                         Output: A Dataframe which has all the missing values imputed.
                                         On Failure: Raise Exception
 
-                                        Written By: iNeuron Intelligence
+                                        Written By: Gourav Singh
                                         Version: 1.0
                                         Revisions: None
                      """
@@ -162,7 +162,7 @@ class Preprocessor:
                                                         Output: A dataframe with scaled
                                                         On Failure: Raise Exception
 
-                                                        Written By: iNeuron Intelligence
+                                                        Written By: Gourav Singh
                                                         Version: 1.0
                                                         Revisions: None
                                      """
@@ -192,7 +192,7 @@ class Preprocessor:
                                                 Output: only the columns with categorical values converted to numerical values
                                                 On Failure: Raise Exception
 
-                                                Written By: iNeuron Intelligence
+                                                Written By: Gourav Singh
                                                 Version: 1.0
                                                 Revisions: None
                              """
@@ -221,7 +221,7 @@ class Preprocessor:
         Output: new balanced feature and target columns
         On Failure: Raise Exception
 
-        Written By: iNeuron Intelligence
+        Written By: Gourav Singh
         Version: 1.0
         Revisions: None
                                      """
@@ -241,4 +241,4 @@ class Preprocessor:
                                        e))
             self.logger_object.log(self.file_object,
                                    'dataset balancing Failed. Exited the handle_imbalanced_dataset method of the Preprocessor class')
-            raise Exception()
+            raise Exception
